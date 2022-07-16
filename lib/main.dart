a7fbd3a848e489ab1718683a47e460d5084c2bf1
chizzo <57238328+tsubachoko@users.noreply.github.com>
2022-07-16 20:08:42 +0900

    ステートフルなウィジェット

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// import 'package:english_words/english_words.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -6,21 +5,36 @@ void main() {
   runApp(const MyApp());
 }
 
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asCamelCase);
+  }
+}
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       )
     );
